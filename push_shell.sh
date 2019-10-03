f0bd5e16565c5d08c4f3b64392e91f9bcf884725
gimoya <kay.cichini@gmail.com>
2019-10-03 23:08:10 +0200

    batch updates from shell script..

--- push_shell.sh
+++ push_shell.sh
@@ -6,6 +6,6 @@ git status
 git commit -m 'batch updates from shell script..'
 echo -e "\n---\n"  
 read -p "Hit [Enter] to continue..."
-git push origin master
+git push origin gh-pages
 echo -e "\n---\n"
 read -p "Hit [Enter] to exit..."
